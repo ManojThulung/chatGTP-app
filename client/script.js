1cde7e49a9b3da0c08c88598bc3208d31d594b94
manoj <manojthulung03@gmail.com>
2023-01-07 13:40:28 +0545

    added functions

---
+++ client/script.js
@@ -0,0 +1,87 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector(".chat-container");
+
+let loadInterval;
+
+//to display loading
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+//to display ai generated text with better ux
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+//to generate unique id for each ai message
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+//to differenciate user and ai message
+function chatStrip(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+        <div class='profile'>
+          <img src='${isAi ? bot : user}' alt='${isAi ? "bot" : "user"} />
+        </div>
+        <div class='message' id='${uniqueId}>
+          ${value}
+        </div>
+      </div>
+    </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //for user chatstripe
+  chatContainer.innerHTML += chatStrip(false, data.get("prompt"));
+  form.reset();
+
+  //for ai chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStrip(true, "", uniqueId);
+
+  //to scroll down the screen automatically to be able to see the message.
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+};
+
+//when the from is submit
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  //13 means Enter key.
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
