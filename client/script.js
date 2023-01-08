8ce5b131d457df0851e46faca5341eafeb9a8a0e
manoj <manojthulung03@gmail.com>
2023-01-08 23:23:12 +0545

    chatstrip function bug fixed and style improved

--- client/script.js
+++ client/script.js
@@ -46,10 +46,10 @@ function chatStrip(isAi, value, uniqueId) {
   return `
     <div class="wrapper ${isAi && "ai"}">
       <div class="chat">
-        <div class='profile'>
+        <div class="profile">
           <img src=${isAi ? bot : user} alt=${isAi ? "bot" : "user"} />
         </div>
-        <div class='message' id='${uniqueId}>
+        <div class="message" id=${uniqueId}>
           ${value}
         </div>
       </div>
